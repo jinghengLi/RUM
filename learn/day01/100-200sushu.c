297881041394ab755562ad293b6a089c7f9b7569
lijingheng <bastusna47518@gmail.com>
2025-08-05 01:55:58 +0800

    1

---
+++ learn/day01/100-200sushu.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int i = 101;
+	while (i < 200)
+	{
+		int sqrt_i = (int)sqrt(i);
+		int flag = 1;
+		for (int j = 2; j <= sqrt_i; j++)
+		{
+			if (i % j == 0)
+			{
+				flag = 0; //如果能被整除，说明不是素数
+				break; //如果能被整除，说明不是素数
+			}
+		}
+		if (flag == 1) //如果没有被整除，说明是素数
+			printf("%d ", i);
+		i += 2;
+	}
+	return 0;
+}
